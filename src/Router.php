5e7c19f4255bb243512212ba2186924951ae6dec
kkekevin <kke_kevin@hotmail.com>
2024-07-12 12:11:56 -0300

    Create layout folder and implement its main render

--- src/Router.php
+++ src/Router.php
@@ -14,11 +14,32 @@ class Router {
         $path = $this->request->getPath();
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
-        if ($callback === false) {
-            echo 'not found';
-            exit;
-        }
-        echo call_user_func($callback);
+        if ($callback === false) 
+            return 'not found';
+        if (is_string($callback))
+            return $this->renderView($callback);
+        return call_user_func($callback);
+    }
+
+    public function renderView($view) 
+    {
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+    }
+
+    protected function layoutContent ()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView ($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean();
     }
 
     public function __construct(\app\Request $request)
