80b1d8bfbc108f21744c1239be0798c81bff4f00
kkekevin <kke_kevin@hotmail.com>
2024-07-18 19:19:58 -0300

    Create form folder

---
+++ src/form/Field.php
@@ -0,0 +1,46 @@
+<?php
+namespace app\form;
+
+use app\Model;
+
+class Field
+{
+    public const TYPE_TEXT = 'text';
+    public const TYPE_PASSWORD = 'password';
+    public const TYPE_NUMBER = 'number';
+
+    public Model $model;
+    public string $attribute, $type;
+
+    public function __toString()
+    {
+        return sprintf('
+            <div class="mb-3">
+                <label>%s</label>
+                <input type="%s" name="%s" value="%s" class="form-control%s">
+            </div>
+            <div class="invalid-feedback">
+                %s
+            </div>
+        ', $this->attribute,
+            $this->type,
+            $this->attribute,
+            $this->model->{$this->attribute},
+            $this->model->hasError($this->attribute) ? ' is-invalid' : '',
+            $this->model->getFirstError($this->attribute)
+        );
+    }
+
+    public function passwordField ()
+    {
+        $this->type = self::TYPE_PASSWORD;
+        return $this;
+    }
+
+    public function __construct(Model $model, string $attribute)
+    {
+        $this->type = self::TYPE_TEXT;
+        $this->model = $model;
+        $this->attribute = $attribute;
+    }
+}
--- src/views/register.php
+++ src/views/register.php
@@ -1,34 +1,16 @@
 <h1>Create an account</h1>
+<?php $form = \app\form\Form::begin('', 'post')?>
+<div class="row">
+    <div class="col">
+        <?php echo $form->field($model, 'firstname') ?>
+    </div>
+    <div class="col">
+        <?php echo $form->field($model, 'lastname') ?>
+    </div>
+</div>
+    <?php echo $form->field($model, 'email') ?>
+    <?php echo $form->field($model, 'password')->passwordField() ?>
+    <?php echo $form->field($model, 'confirmPasswor')->passwordField() ?>
 
-<form action="" method="post">
-    <div class="row">
-        <div class="col">
-            <div class="mb-3">
-                <label>Firstname</label>
-                <input type="text" name="firstname" class="form-control <?php echo $model->hasError('firstname') ? ' is-invalid' : '' ?>">
-                <div class="invalid-feedback">
-                    <?php echo $model->getFirstError($firstname) ?>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="mb-3">
-                <label>Lastname</label>
-                <input type="text" name="lastname" class="form-control">
-            </div>
-        </div>
-    </div>
-    <div class="mb-3">
-        <label>Email</label>
-        <input type="email" name="email" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label>Password</label>
-        <input type="password" name="password" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label>Confirm Password</label>
-        <input type="password" name="confirmPassword" class="form-control">
-    </div>
     <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<?php \app\form\Form::end() ?>
